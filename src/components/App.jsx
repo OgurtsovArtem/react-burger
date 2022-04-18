8dcec033c05e4b1f6552dd901c88a5569fc1b2d9
Ogurtsov Artem <ogurcov.artem2012@list.ru>
2022-04-18 18:39:45 +0300

    замена компонента на родительский для BurgerConstructor и BurgerIngredients

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import AppHeader from "./AppHeader/AppHeader";
+import BurgerBody from "./BurgerBody/BurgerBody";
 import "./App.css";
 
 function App() {
   return (
     <>
       <AppHeader />
+      <BurgerBody />
     </>
   );
 }
