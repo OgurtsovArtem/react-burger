d19ff23b72ff5b3d804025a4a88a51dd6ec999f7
Ogurtsov Artem <ogurcov.artem2012@list.ru>
2022-04-19 18:44:06 +0300

    адаптив/логика

--- src/components/IngredientsCard/IngredientsCard.jsx
+++ src/components/IngredientsCard/IngredientsCard.jsx
@@ -1,22 +1,34 @@
+import React from "react";
 import IngredientsCardStyle from "./IngredientsCard.module.css";
-import { CurrencyIcon } from "@ya.praktikum/react-developer-burger-ui-components";
+import { CurrencyIcon, Counter } from "@ya.praktikum/react-developer-burger-ui-components";
+import PopupIngredients from "../PopupIngredients/PopupIngredients";
 
 function IngredientsCard(props) {
+  const [activePopup, setActivePopup] = React.useState(false);
+  const changePopupStatus = () => {
+    setActivePopup(!activePopup);
+  };
   return (
-    <div className={IngredientsCardStyle.card}>
-      <picture className={`${IngredientsCardStyle.image} ml-4 mr-4 mb-1`}>
-        <source media="(min-width: 1024px)" srcSet={props.image_large} />
-        <source media="(min-width: 539px)" srcSet={props.image} />
-        <img src={props.image_mobile} alt={props.name} />
-      </picture>
-      <div className={`${IngredientsCardStyle.price} mt-1 mb-1`}>
-        <span className={`${IngredientsCardStyle.price} text text_type_digits-default`}>
-          {props.price}
-        </span>
-        <CurrencyIcon type="primary" />
+    <>
+      {activePopup && <PopupIngredients onClick={changePopupStatus} data={props} />}
+      <div className={IngredientsCardStyle.card} onClick={changePopupStatus}>
+        <Counter count={1} size="default" />
+        <picture>
+          <source media="(min-width: 1024px)" srcSet={props.image_large} />
+          <source media="(min-width: 539px)" srcSet={props.image} />
+          <img
+            className={`${IngredientsCardStyle.image} ml-4 mr-4 mb-1`}
+            src={props.image_mobile}
+            alt={props.name}
+          />
+        </picture>
+        <div className={`${IngredientsCardStyle.price} mt-1 mb-1`}>
+          <span className={`text text_type_digits-default  mr-1`}>{props.price}</span>
+          <CurrencyIcon type="primary" />
+        </div>
+        <h3 className={`'text text_type_main-default'`}>{props.name}</h3>
       </div>
-      <h3 className={`${IngredientsCardStyle.name} 'text text_type_main-default'`}>{props.name}</h3>
-    </div>
+    </>
   );
 }
 
