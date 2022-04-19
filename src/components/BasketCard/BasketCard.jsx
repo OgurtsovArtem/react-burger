d496a12a71d789b6086179af30a95f23aae6851d
Ogurtsov Artem <ogurcov.artem2012@list.ru>
2022-04-19 18:28:22 +0300

    адаптив + логика отрисовки

--- src/components/BasketCard/BasketCard.jsx
+++ src/components/BasketCard/BasketCard.jsx
@@ -1,19 +1,36 @@
 import React from "react";
-import { DeleteIcon, LockIcon, DragIcon } from "@ya.praktikum/react-developer-burger-ui-components";
+import {
+  DeleteIcon,
+  LockIcon,
+  DragIcon,
+  CurrencyIcon,
+} from "@ya.praktikum/react-developer-burger-ui-components";
 import BasketCardStyle from "./BasketCard.module.css";
 
 function BasketCard(props) {
-  const [status, setStatus] = React.useState(false);
+  const [status, setStatus] = React.useState(true);
+
+  const changeStatus = () => {
+    setStatus(!status);
+  };
+
   return (
-    <div className={BasketCardStyle.card}>
-      <DragIcon type="primary" />
-      <div className={BasketCardStyle.info}>
-        <img className={BasketCardStyle.image} src={props.image} alt={props.name} />
-        <p className={BasketCardStyle.name}>{props.name}</p>
+    <div className={`${BasketCardStyle.card} mb-4 mr-2`}>
+      <button>
+        <DragIcon type="primary" />
+      </button>
+      <div className={`${BasketCardStyle.info} pt-4 pb-4 pl-6 pr-8`}>
+        <img className={BasketCardStyle.image} src={props.image_mobile} alt={props.name} />
+        <p className={`${BasketCardStyle.name} text text_type_main-default`}>{props.name}</p>
         <div className={BasketCardStyle.price}>
-          <span className={BasketCardStyle.value}>{props.price}</span>
-          {status ? <DeleteIcon type="primary" /> : <LockIcon type="primary" />}
+          <span className={`${BasketCardStyle.value} text text_type_digits-default`}>
+            {props.price}
+          </span>
+          <CurrencyIcon type="primary" />
         </div>
+        <button className={BasketCardStyle.statusButton}>
+          {status ? <DeleteIcon type="primary" /> : <LockIcon type="primary" />}
+        </button>
       </div>
     </div>
   );
