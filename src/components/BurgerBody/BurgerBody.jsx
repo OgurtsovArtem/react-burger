e90eb3f2a5ef5ebaa15036bc4c06dc691a2c87f1
Ogurtsov Artme <ogurcov.artem2012@list.ru>
2022-04-23 15:11:12 +0300

    Убраны хардкод данные добавлено обращение к API

--- src/components/BurgerBody/BurgerBody.jsx
+++ src/components/BurgerBody/BurgerBody.jsx
@@ -1,15 +1,48 @@
+import React from "react";
 import BurgerBodyStyle from "./BurgerBody.module.css";
 
 import BurgerConstructor from "../BurgerConstructor/BurgerConstructor";
 import BurgerIngredients from "../BurgerIngredients/BurgerIngredients";
+import Loader from "../Loader/Loader";
 
-import data from "../../utils/data.json";
+const DATA_URL = "https://norma.nomoreparties.space/api/ingredients";
 
 function BurgerBody() {
+  const [state, setState] = React.useState({
+    success: false,
+    data: [],
+  });
+
+  const getData = () => {
+    fetch(DATA_URL)
+      .then((res) => {
+        if (!res.ok) {
+          throw res;
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setState({ ...state, data: data.data, success: true });
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  React.useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <section className={BurgerBodyStyle.section}>
-      <BurgerIngredients data={data} />
-      <BurgerConstructor data={data} />
+      {state.success ? (
+        <>
+          <BurgerIngredients data={state.data} />
+          <BurgerConstructor data={state.data} />
+        </>
+      ) : (
+        <Loader />
+      )}
     </section>
   );
 }
