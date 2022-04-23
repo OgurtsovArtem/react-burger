51466f9abcb953e288441c8f2576e77f1eccafdc
Ogurtsov Artme <ogurcov.artem2012@list.ru>
2022-04-23 15:02:21 +0300

    перенес компонент app в отдельную папку

---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import AppHeader from "../AppHeader/AppHeader";
+import BurgerBody from "..//BurgerBody/BurgerBody";
+
+function App() {
+  return (
+    <>
+      <AppHeader />
+      <BurgerBody />
+    </>
+  );
+}
+
+export default App;
