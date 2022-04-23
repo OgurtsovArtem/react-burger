6f707178f5186840028e89de4130380c8ce178d6
Ogurtsov Artme <ogurcov.artem2012@list.ru>
2022-04-23 15:09:13 +0300

    перенесена логика из Ui компонента

--- src/components/BasketCard/BasketCard.jsx
+++ src/components/BasketCard/BasketCard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import clsx from "clsx";
 import {
   DeleteIcon,
   LockIcon,
@@ -7,29 +7,36 @@ import {
 } from "@ya.praktikum/react-developer-burger-ui-components";
 import BasketCardStyle from "./BasketCard.module.css";
 
-function BasketCard(props) {
-  const [status, setStatus] = React.useState(true);
+function BasketCard({ isLocked, data, type }) {
+  const { name, image_mobile, price, handleClose } = data;
 
-  const changeStatus = () => {
-    setStatus(!status);
-  };
+  const className = clsx(BasketCardStyle.info, {
+    [BasketCardStyle.info_top]: type === "top",
+    [BasketCardStyle.info_middle]: type === "middle",
+    [BasketCardStyle.info_bottom]: type === "bottom",
+  });
 
   return (
     <div className={`${BasketCardStyle.card} mb-4 mr-2`}>
-      <button>
-        <DragIcon type="primary" />
-      </button>
-      <div className={`${BasketCardStyle.info} pt-4 pb-4 pl-6 pr-8`}>
-        <img className={BasketCardStyle.image} src={props.image_mobile} alt={props.name} />
-        <p className={`${BasketCardStyle.name} text text_type_main-default`}>{props.name}</p>
+      {type === "middle" ? (
+        <button>
+          <DragIcon type="primary" />
+        </button>
+      ) : null}
+
+      <div className={`${className} pt-4 pb-4 pl-6 pr-8`}>
+        <img className={BasketCardStyle.image} src={image_mobile} alt={name} />
+        <p className={`${BasketCardStyle.name} text text_type_main-default`}>{name}</p>
         <div className={BasketCardStyle.price}>
-          <span className={`${BasketCardStyle.value} text text_type_digits-default`}>
-            {props.price}
-          </span>
+          <span className={`${BasketCardStyle.value} text text_type_digits-default`}>{price}</span>
           <CurrencyIcon type="primary" />
         </div>
         <button className={BasketCardStyle.statusButton}>
-          {status ? <DeleteIcon type="primary" /> : <LockIcon type="primary" />}
+          {isLocked ? (
+            <LockIcon type="primary" />
+          ) : (
+            <DeleteIcon type="primary" onClick={handleClose} />
+          )}
         </button>
       </div>
     </div>
