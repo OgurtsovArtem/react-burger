cddff89a56ea05b5ca45bcca6c958d428ea9564e
Ogurtsov Artem <ogurcov.artem2012@list.ru>
2022-06-09 19:18:39 +0300

    логика открывания модалки

--- src/components/IngredientsCard/IngredientsCard.jsx
+++ src/components/IngredientsCard/IngredientsCard.jsx
@@ -6,17 +6,15 @@ import {
   SET_DETAIL_INGREDIENTS,
   DELETE_DETAIL_INGREDIENTS,
 } from "../../services/actions/ingredients";
-import IngredientDetails from "../IngredientDetails/IngredientDetails";
-import Modal from "../Modal/Modal";
 import ingredientsPropTypes from "../../utils/types";
 import { useDrag } from "react-dnd";
-import { Link, useRouteMatch } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 function IngredientsCard({ data }) {
   const [activePopup, setActivePopup] = React.useState(false);
+  const location = useLocation();
   const dispatch = useDispatch();
-  const { url } = useRouteMatch();
-  console.log(url, data._id);
+  // const { url } = useRouteMatch();
   const [, dragRef] = useDrag({
     type: "ingredient",
     item: { id: data._id, type: data.type },
@@ -33,33 +31,26 @@ function IngredientsCard({ data }) {
     }
   };
   return (
-    <>
-      {activePopup && (
-        <Modal onClose={changePopupStatus} header={"Детали ингредиента"}>
-          <IngredientDetails />
-        </Modal>
-      )}
-      <Link to={`${url}/${data._id}`}>
-        <div ref={dragRef} className={IngredientsCardStyle.card} onClick={changePopupStatus}>
-          {data?.qty ? <Counter count={data.qty} size="default" /> : null}
+    <Link to={{ pathname: `/ingredients/${data._id}`, state: { background: location } }}>
+      <div ref={dragRef} className={IngredientsCardStyle.card} onClick={changePopupStatus}>
+        {data?.qty ? <Counter count={data.qty} size="default" /> : null}
 
-          <picture>
-            <source media="(min-width: 1024px)" srcSet={data.image_large} />
-            <source media="(min-width: 539px)" srcSet={data.image} />
-            <img
-              className={`${IngredientsCardStyle.image} ml-4 mr-4 mb-1`}
-              src={data.image_mobile}
-              alt={data.name}
-            />
-          </picture>
-          <div className={`${IngredientsCardStyle.price} mt-1 mb-1`}>
-            <span className={`text text_type_digits-default  mr-1`}>{data.price}</span>
-            <CurrencyIcon type="primary" />
-          </div>
-          <h3 className="text text_type_main-default">{data.name}</h3>
+        <picture>
+          <source media="(min-width: 1024px)" srcSet={data.image_large} />
+          <source media="(min-width: 539px)" srcSet={data.image} />
+          <img
+            className={`${IngredientsCardStyle.image} ml-4 mr-4 mb-1`}
+            src={data.image_mobile}
+            alt={data.name}
+          />
+        </picture>
+        <div className={`${IngredientsCardStyle.price} mt-1 mb-1`}>
+          <span className={`text text_type_digits-default  mr-1`}>{data.price}</span>
+          <CurrencyIcon type="primary" />
         </div>
-      </Link>
-    </>
+        <h3 className="text text_type_main-default">{data.name}</h3>
+      </div>
+    </Link>
   );
 }
 
