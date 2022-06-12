651a94517e0dcaa44239df9300e8efd3e74d1800
Ogurtsov Artme <ogurcov.artem2012@list.ru>
2022-06-12 16:34:29 +0300

    новые файлы

---
+++ src/services/reducers/user.js
@@ -0,0 +1,160 @@
+import {
+  AUTH_CHECKED,
+  USER_LOGOUT,
+
+  LOGIN_USER_REQUEST,
+  LOGIN_USER_FAILED,
+  LOGIN_USER_SUCCESS,
+
+  REGISTER_USER_REQUEST,
+  REGISTER_USER_FAILED,
+  REGISTER_USER_SUCCESS,
+
+  GET_USER_REQUEST,
+  GET_USER_FAILED,
+  GET_USER_SUCCESS,
+
+  UPDATE_USER_REQUEST,
+  UPDATE_USER_FAILED,
+  UPDATE_USER_SUCCESS,
+} from '../actions/user';
+
+const initialState = {
+  isAuthCheck: false,
+
+  sending: false,
+
+  data: null,
+
+  loginUserFailed: null,
+  loginUserRequest: false,
+
+  registerUserFailed: null,
+  registerUserRequest: false,
+
+  getUserFailed: null,
+  getUserRequest: false,
+
+  updateUserFailed: null,
+  updateUserRequest: false,
+}
+
+export const userReducer = (state = initialState, action) => {
+switch (action.type) {
+  case AUTH_CHECKED: {
+    return {
+      ...state,
+      isAuthCheck: true,
+    };
+  }
+  case USER_LOGOUT: {
+    return {
+      ...state,
+      data: null,
+      isAuthCheck: false,
+    };
+  }
+    case LOGIN_USER_REQUEST: {
+      return {
+        ...state,
+        loginUserRequest: true,
+        loginUserFailed: null,
+        sending: true,
+      };
+    }
+    case LOGIN_USER_FAILED: {
+      return {
+        ...state,
+        loginUserRequest: false,
+        loginUserFailed: action.error,
+        sending: false,
+      };
+    }
+    case LOGIN_USER_SUCCESS: {
+      return {
+        ...state,
+        loginUserRequest: false,
+        data: action.data,
+        isAuthCheck: true,
+        sending: false,
+      };
+    }
+    case REGISTER_USER_REQUEST: {
+      return {
+        ...state,
+        registerUserRequest: true,
+        registerUserFailed: null,
+        sending: true,
+      };
+    }
+    case REGISTER_USER_FAILED: {
+      return {
+        ...state,
+        registerUserRequest: false,
+        registerUserFailed: action.error,
+        sending: false,
+      };
+    }
+    case REGISTER_USER_SUCCESS: {
+      return {
+        ...state,
+        registerUserRequest: false,
+        sending: false,
+      };
+    }
+
+    case GET_USER_REQUEST: {
+      return {
+        ...state,
+        getUserRequest: true,
+        getUserFailed: null,
+        sending: true,
+      };
+    }
+    case GET_USER_FAILED: {
+      return {
+        ...state,
+        getUserRequest: false,
+        getUserFailed: action.error,
+        sending: false,
+      };
+    }
+    case GET_USER_SUCCESS: {
+      return {
+        ...state,
+        getUserRequest: false,
+        data: action.data,
+        isAuthCheck: true,
+        sending: false,
+      };
+    }
+
+    case UPDATE_USER_REQUEST: {
+      return {
+        ...state,
+        updateUserRequest: true,
+        updateUserFailed: null,
+        sending: true,
+      };
+    }
+    case UPDATE_USER_FAILED: {
+      return {
+        ...state,
+        updateUserRequest: false,
+        updateUserFailed: action.error,
+        sending: false,
+      };
+    }
+    case UPDATE_USER_SUCCESS: {
+      return {
+        ...state,
+        updateUserRequest: false,
+        data: action.data,
+        sending: false,
+      };
+    }
+    default: {
+        return state
+    }
+  }
+} 
